38919261a69745249b2b272a5cd06237e3f7cab7
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-11-28 23:44:19 +0000

    bot only responds to messages in the science channels

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 // BotID will be the alias for the bot
 var BotID string
 var token string = os.Getenv("TOKEN")
+var channels = [...]string{"773318228184137759", "773318250805198848", "773318284325027883"}
 
 func openConnection() {
 	dg, err := discordgo.New("Bot " + token)
@@ -44,10 +45,14 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == BotID {
 		return
 	}
-	if m.Content == "!osmosis" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "osmosis is the net movement of water against the conc. gradient")
+	// make sure the bot only replies in science channels
+	for i := 0; i < len(channels); i++ {
+		if m.ChannelID == channels[i] {
+			if m.Content == "!osmosis" {
+				_, _ = s.ChannelMessageSend(m.ChannelID, "osmosis is the net movement of water against the conc. gradient")
+			}
+		}
 	}
-	fmt.Println(m.Content)
 }
 
 func main() {
